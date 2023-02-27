52c5a1a42b593d9d793249df9d0ecd6f7ed334ab
Jiripoz <103076776+Jiripoz@users.noreply.github.com>
2023-02-27 01:18:24 -0300

    minor changes to di.ts

--- src/stores/di.ts
+++ src/stores/di.ts
@@ -3,32 +3,69 @@ import { ChessComBridge } from "src/bridges/chesscom.bridge";
 
 export let bridge: ChessPiecesBridge;
 
-export const setupDependencies = async () => {
+let boardObserver: MutationObserver;
+let headerObserver: MutationObserver;
+let mutationChangeCount = 0;
+
+const disconnectObserver = (observer, observerName) => {
+  if (observer) {
+    console.log(`[disconnectObserver] DISCONNECTING: ${observerName}`);
+    observer?.disconnect();
+  }
+};
+
+const updateBoardObserver = (callback, targetNode) => {
+  console.log("[updateBoardObserver] starting");
+  const config = { attributes: true, childList: true, subtree: true };
+  disconnectObserver(boardObserver, "boardObserver");
+  boardObserver = undefined;
+  boardObserver = new MutationObserver(callback);
+  boardObserver.observe(targetNode, config);
+};
+
+const setupObservers = () => {
+  console.log("[setupObserver] starting");
+  disconnectObserver(boardObserver, "boardObserver");
+  disconnectObserver(headerObserver, "headerObserver");
+
   const website = document.location.origin;
   if (website.includes("chess.com")) {
     bridge = new ChessComBridge();
   } else if (website.includes("lichess.org")) {
     bridge = new ChessComBridge();
   } else {
+    return;
   }
-  bridge.updateChessPieces();
 
-  const targetNode = document.getElementsByTagName(bridge.tagName)[0];
-  const config = { attributes: true, childList: true, subtree: true };
+  headerObserver = new MutationObserver(() => {
+    console.log("[headerObserver] starting");
+    disconnectObserver(boardObserver, "boardObserver");
 
-  let observer: MutationObserver;
+    const targetNode = document.getElementsByTagName(bridge.tagName)[0];
+    if (targetNode) {
+      bridge.updateChessPieces();
 
-  const callback = async (mutationList) => {
-    console.log("entrei no callback");
-    observer.disconnect();
-    bridge.updateChessPieces();
+      const boardCallBack = async (_) => {
+        console.log("[boardCallBack] starting");
+        mutationChangeCount += 1;
+        disconnectObserver(boardObserver, "boardObserver");
+        bridge.updateChessPieces();
 
-    await bridge.updateHighlights();
+        await bridge.updateHighlights();
+        updateBoardObserver(boardCallBack, targetNode);
 
-    observer = new MutationObserver(callback);
-    observer.observe(targetNode, config);
-  };
+        console.log(`Mutation Change Count: ${mutationChangeCount}`);
+      };
+      updateBoardObserver(boardCallBack, targetNode);
+    }
+  });
 
-  observer = new MutationObserver(callback);
-  observer.observe(targetNode, config);
+  headerObserver.observe(document.head, { attributes: true, childList: true, subtree: true });
+};
+
+export const setupDependencies = async () => {
+  window.addEventListener("locationchange", function () {
+    setupObservers();
+  });
+  setupObservers();
 };
